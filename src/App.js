7ad93a31c047f1c18efd467d2e8fc38d3793053f
Vimukthi Gunasekara <vimukthisadaruwan65@gmail.com>
2023-02-26 16:28:36 -0500

    add drag and drop item/functions

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+import React, { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { v4 as uuid } from 'uuid';
+
+const itemsFromBackend = [
+  { id: uuid(), content: "01 task" },
+  { id: uuid(), content: "02 task" },
+  { id: uuid(), content: "03 task" }
+];
+
+const columnsFromBackend = {
+  [uuid()]: {
+    name: "To do",
+    items: itemsFromBackend
+  }
+};
 
 
 function App() {
