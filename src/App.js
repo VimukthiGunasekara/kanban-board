cacbe7d9c0762e35deb8b9f381dbd98558b574e8
Vimukthi Gunasekara <vimukthisadaruwan65@gmail.com>
2023-02-26 16:58:32 -0500

    rendaring moveable items

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const columnsFromBackend = {
 };
 
 const onDragEnd = (result, columns, setColumns) => {
-  
+
 };
 
 function App() {
@@ -24,9 +24,19 @@ function App() {
   return (
     <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
       <DragDropContext onDragEnd={result => onDragEnd(result, columns, setColumns)}>
-      {Object.entries(columns).map(([columnId, column], index) => {
+        {Object.entries(columns).map(([columnId, column], index) => {
           return (
-            <Droppable droppableId={columnId} key={columnId}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center"
+              }}
+              key={columnId}
+            >
+              <h2>{column.name}</h2>
+              <div style={{ margin: 8 }}>
+                <Droppable droppableId={columnId} key={columnId}>
                   {(provided, snapshot) => {
                     return (
                       <div
@@ -36,18 +46,52 @@ function App() {
                           background: snapshot.isDraggingOver ? "lightblue" : "lightgrey",
                           padding: 4,
                           width: 250,
-                          minHeight: 500
+                          minHeight: 600
                         }}
-                      >                      
-                       
+                      >
+                        {column.items.map((item, index) => {
+                          return (
+                            <Draggable
+                              key={item.id}
+                              draggableId={item.id}
+                              index={index}
+                            >
+                              {(provided, snapshot) => {
+                                return (
+                                  <div
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                    style={{
+                                      userSelect: "none",
+                                      padding: 16,
+                                      margin: "0 0 8px 0",
+                                      minHeight: "50px",
+                                      backgroundColor: snapshot.isDragging
+                                        ? "#263B4A"
+                                        : "#456C86",
+                                      color: "white",
+                                      ...provided.draggableProps.style
+                                    }}
+                                  >
+                                    {item.content}
+                                  </div>
+                                );
+                              }}
+                            </Draggable>
+                          );
+                        })}
+                        {provided.placeholder}
                       </div>
                     );
                   }}
                 </Droppable>
-          )
+              </div>
+            </div>
+          );
         })}
-      </DragDropContext>
-    </div>
+      </DragDropContext >
+    </div >
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
